947e0356b46039b10fa4315203e500a557cf98e8
Debangi-Dey <74201092+Debangi-Dey@users.noreply.github.com>
2022-05-07 14:44:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,47 @@
 img = ""
-status1= ""
+status1 = ""
+Objects = []
+
 function preload() {
     img = loadImage("dog_cat.jpg")
 }
 
 function setup() {
-    canvas = createCanvas(500, 430)
+    canvas = createCanvas(600, 500)
     canvas.center()
-    objD=ml5.objectDetector('cocossd', modelLoaded)
-    document.getElementById("status").innerHTML="status : detecting object"
+    objD = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "status : detecting object"
 }
 
-function modelLoaded(){
+function modelLoaded() {
     console.log("model Loaded")
-status1=true
-objD.detect(img, gotResults)
+    status1 = true
+    objD.detect(img, gotResults)
 }
 
-function gotResults(error, results){
-if (error) {
-    console.error(error)
-}
-else {
-    console.log(results)
-}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.log(results)
+        Objects = results
+    }
 }
 
 function draw() {
-    image(img, 0, 0, 500, 500)
+    image(img, 0, 0, 600, 500)
+    if (status1 != "") {
+        for (i = 0; i < Objects.length; i++) {
+            document.getElementById("status").innerHTML = "status : object detected"
+            fill("red")
+            perc = floor(Objects[i].confidence * 100)
+            text(Objects[i].label + " " + perc + "%", Objects[i].x-30, Objects[i].y)
+            noFill()
+            stroke("pink")
+            rect(Objects[i].x-30, Objects[i].y, Objects[i].width, Objects[i].height)
+        }
+    }
+    /*
     text("dog", 45, 45)
     fill("red")
     noFill()
@@ -38,4 +52,5 @@ function draw() {
     noFill()
     stroke("brown")
     rect(200, 60, 250, 350)
+    */
 }
